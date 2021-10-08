4aa16f95619d22006626278a0d3bfbf2cc26d71a
Sean <sdillon215@gmail.com>
2021-10-07 20:38:40 -0600

    duplicte tests

--- lesson.js
+++ lesson.js
@@ -25,3 +25,41 @@ $('#isEvenSubmit').click(function(event) {
         console.log('Odd');
       }
 });
+
+// search algorithm to find the index of number passed into funtion
+function findIndex(arrNum) {
+  const data = [12, 23, 38, 40, 54, 62, 71, 87, 99];
+  for (let i = 0; i < data.length; i++) {
+    if (arrNum === data[i]) {
+      console.log(`${arrNum} found at index ${i}`);
+    }
+  }
+};
+
+findIndex(38);
+
+// binary search
+const data = [12, 23, 38, 40, 54, 62, 71, 87, 99];
+
+const binarySearch = (arr, num, left, right) => {
+  let middle = Math.floor((left + right) / 2);
+
+  // range overlapped, so never found number
+  if (left > right) {
+    return -1;
+  }
+  else if (num === arr[middle]) {
+    return middle;
+  }
+  else if (num < arr[middle]) {
+    // call again with a new right value
+    return binarySearch(arr, num, left, middle - 1);
+  }
+  else {
+    // call again with a new left value
+    return binarySearch(arr, num, middle + 1, right);
+  }
+};
+
+// set initial left and right values on first call
+console.log(binarySearch(data, 12, 0, data.length - 1));
